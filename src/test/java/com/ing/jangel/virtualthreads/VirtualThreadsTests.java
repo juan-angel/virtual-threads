60b4daa42287c0b39cf9b58b54f68a8726e09858
juan-angel <juan.angel.gascon@gmail.com>
2023-11-02 17:57:48 +0100

    Initial commit

---
+++ src/test/java/com/ing/jangel/virtualthreads/VirtualThreadsTests.java
@@ -0,0 +1,14 @@
+package com.ing.jangel.virtualthreads;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VirtualThreadsTests {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
